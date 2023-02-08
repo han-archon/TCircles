f76e4b167b90bea43475e93e829b61020370988b
hanjaewon <jaewonhan@amanda.co.kr>
2023-02-08 23:49:40 +0900

    :white_check_mark:  circles 모델 수정

--- tcircles/app/model/circles_model.js
+++ tcircles/app/model/circles_model.js
@@ -2,15 +2,15 @@
 const db = require('../../dbConnect/dbConnect');
 
 //생성자
-const circlesList = ((circlesList) => {
-   this.circlesMemberId  = circlesList.member_id;
-   this.circlesName      = circlesList.circles_name;
-   this.circlesContents  = circlesList.circles_contents;
-   this.circlesTypeId    = circlesList.type_id;
-   this.circlesJoinLimt  = circlesList.circles_join_limit;
-   this.circlesIsPrivate = circlesList.circles_private_yn;              // 방 공개 여부
-   this.circlesPassword  = circlesList.circles_private_password;        // 방 비공개 시 방 비밀번호
-   this.circlesRegdate   = circlesList.regdate;
+const circlesList = ((circles) => {
+   this.member_id                 = circles.member_id;
+   this.circles_name              = circles.circles_name;
+   this.circles_contents          = circles.circles_contents;
+   this.type_id                   = circles.type_id;
+   this.circles_join_limit        = circles.circles_join_limit;
+   this.circles_private_yn        = circles.circles_private_yn;              // 방 공개 여부
+   this.circles_private_password  = circles.circles_private_password;        // 방 비공개 시 방 비밀번호
+   this.regdate                   = circles.regdate;
 });
 
 const allListQuery = "SELECT *  FROM circles";
