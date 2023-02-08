2b699b8966f567525fc2f31092fbbb3c530275db
hanjaewon <jaewonhan@amanda.co.kr>
2023-02-09 00:20:24 +0900

    :mute:  controller log 제거

--- tcircles/app/controller/circles_controller.js
+++ tcircles/app/controller/circles_controller.js
@@ -1,7 +1,6 @@
 const circlesModel = require("../model/circles_model");
 
 exports.getCirclesList = (req, res, next) => {
-    console.log("controller 진입");
     circlesModel.allList((err, circlesList) => {
        if (err) throw  err;
        res.send(circlesList);
