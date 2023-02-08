195f60c2b52cc0a04ecc7f91bb233481207efc26
hanjaewon <jaewonhan@amanda.co.kr>
2023-02-09 00:03:57 +0900

    :loud_sound:  controller log 추가

--- tcircles/app/controller/circles_controller.js
+++ tcircles/app/controller/circles_controller.js
@@ -1,6 +1,7 @@
 const circlesModel = require("../model/circles_model");
 
 exports.getCirclesList = (req, res, next) => {
+    console.log("controller 진입");
     circlesModel.allList((err, circlesList) => {
        if (err) throw  err;
        res.send(circlesList);
